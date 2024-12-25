95bb690ffbc4402dca9cccc79e8403a3a82086d0
Amy <92268808+AmyVerse@users.noreply.github.com>
2024-12-25 12:10:44 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
 function App() {
   return (
